fbfd2df9d0e1f31ed63ecbfc4013aab1871c5385
shichuyu <1042048096@qq.com>
2021-06-08 00:20:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  // 向这个 class 中添加一个构造函数，用来初始化 state
+  /* 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
+   因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。 */
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick={()=>{ this.setState({value: 'X'})} }>
+        {/* TODO */}
+        {/* {this.props.value} */}
+        {this.state.value}
+      </button>
+    );
+  }
   }
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
