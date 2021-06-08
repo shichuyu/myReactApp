992818f43a8cd1454d1011b26552169da5c7a39c
shichuyu <1042048096@qq.com>
2021-06-08 23:48:43 +0800

    井字棋游戏

--- src/index.js
+++ src/index.js
@@ -1,78 +1,139 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  // 向这个 class 中添加一个构造函数，用来初始化 state
-  /* 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
-   因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。 */
+// class Square extends React.Component {
+//   // 向这个 class 中添加一个构造函数，用来初始化 state
+//   /* 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
+//    因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。 */
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null,
+
+//     };
+//   }
+//   render() {
+//     return (
+//       <button className="square" onClick={()=>{ 
+//         this.props.onClick() // Board 传递（onClick={() => this.handleClick(i)}方法）给当前的 Square ，Square onClick触发 即触发 this.handleClick();
+//       } }>
+//         {/* TODO */}
+//         {this.props.value}
+//         {/* {this.state.value} */}
+//       </button>
+//     );
+//   }
+// }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+  
+class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: null,
-    };
+    this.state={
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
   }
-  render() {
+  renderSquare(i) {
     return (
-      <button className="square" onClick={()=>{ this.setState({value: 'X'})} }>
-        {/* TODO */}
-        {/* {this.props.value} */}
-        {this.state.value}
-      </button>
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
     );
   }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); // 每次点击则拷贝一个副本，并将对应点击的值 改变 squares[i] == 'changeVal' X or O
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  render() {
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
-      );
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// 判断出胜者
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
+  return null;
+}
   
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
   
